31f66ff6cc49f4f166e81d9b6590ebddc9bee49f
16ur <akuseruuu@protonmail.com>
2024-09-25 23:43:23 +0200

    Fix fav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import CoreContent from "./components/CoreContent/CoreContent";
